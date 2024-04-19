3f36934a6389f2aa04746af182852787d6d77649
Josué Antonio Contreras Guzmán <antonioguzman_dev@hotmail.com>
2024-04-18 23:01:06 -0600

    mi primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let hamburguer = document.querySelector('#hamburger');
+    hamburguer.addEventListener('click', () => {
+        navbar.classList.toggle('open');
+    });
+})()
+
+
